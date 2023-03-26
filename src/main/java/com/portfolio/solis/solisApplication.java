dbe074bc6283d489f5dc2ee2d9ecce1d51eb3e76
arielSolis <108560263+ArielS76@users.noreply.github.com>
2023-03-26 17:49:16 -0300

    Add files via upload

---
+++ src/main/java/com/portfolio/solis/solisApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.solis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class solisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(solisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/solis/SolisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.solis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
